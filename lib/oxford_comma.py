139baf2998634de9c8de5634d7f17070e7a09c5b
DevBrianHQ <kipchumbabrian47@gmail.com>
2024-09-27 18:49:00 +0300

    passed test

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
+
+print(oxford_comma(["kiwi"])) 
+print(oxford_comma(["kiwi", "durian"]))
+print(oxford_comma(["kiwi", "durian", "starfruit"]))
+print(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]))
